5252739b87088ac6215b2308669a2aec27059689
yochin <yochin47@gmail.com>
2021-01-14 13:16:17 +0900

    add debugging print

--- _init_paths.py
+++ _init_paths.py
@@ -10,6 +10,15 @@ this_dir = osp.dirname(__file__)
 # Add lib to PYTHONPATH
 lib_path = osp.join(this_dir, 'lib')
 add_path(lib_path)
+# print('add_path: ', lib_path)
 
 coco_path = osp.join(this_dir, 'data', 'coco', 'PythonAPI')
 add_path(coco_path)
+# print('add_path: ', coco_path)
+
+# sys.path.remove('/home/yochin/Desktop/DA_Detection_py1/lib')
+
+# print('in sys.path:', )
+# for p in sys.path:
+#     print(p)
+
